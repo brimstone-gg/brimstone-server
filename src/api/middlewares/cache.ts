462c33f4c7c530591698b0ecba495a149c8a5691
Jan Henning <triton539@hotmail.com>
2020-08-06 06:46:45 +0200

    ci test

--- src/api/middlewares/cache.ts
+++ src/api/middlewares/cache.ts
@@ -5,11 +5,7 @@ const REDIS_PORT = parseInt(process.env.REDIS_PORT) || 6379
 const REDIS_HOST = process.env.NODE_ENV === 'production' ? process.env.REDIS_HOST : '127.0.0.1'
 const REDIS_PASSWORD = process.env.NODE_ENV === 'production' ? process.env.REDIS_PASSWORD : ''
 
-export const redisClient = redis.createClient({
-  host: REDIS_HOST,
-  port: REDIS_PORT,
-  password: REDIS_PASSWORD
-})
+export const redisClient = redis.createClient({ host: REDIS_HOST, port: REDIS_PORT, password: REDIS_PASSWORD })
 
 export const accountsByRiotIdCache = (req: Request, res: Response, next: NextFunction) => {
   const { region, username, discriminator } = req.params
